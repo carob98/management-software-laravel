1a718e0334b0cc66655ab947af8efaa20a173d0c
carob98 <caro.bon98@gmail.com>
2025-09-17 11:26:29 +0200

    Project setup: Bootstrap, MySQL DB, Laravel Fortify

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>{{ env('APP_NAME') }}</title>
+    @vite (['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body>
+    <header>
+        <x-nav></x-nav>
+    </header>
+    <div class="container.fluid full-bg p-5">
+        {{ $slot }}
+    </div>
+
+    <x-footer></x-footer>
+</body>
+
+</html>
