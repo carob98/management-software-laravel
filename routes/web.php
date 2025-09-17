d5d5354288246d4e4322b5fd2c5ec9c07dc75270
carob98 <caro.bon98@gmail.com>
2025-09-17 12:47:27 +0200

    middleware to access CRUD routes

--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PageController::class, 'homepage'])->name('pages.homepage');
 
-Route::resource('products', ProductController::class);
+Route::resource('products', ProductController::class)->middleware('auth');
